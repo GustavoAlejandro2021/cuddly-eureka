c96d06e888c6e6aff0ab5645b6e6e5cce7a74e74
GustavoAlejandro2021 <80475032+GustavoAlejandro2021@users.noreply.github.com>
2021-03-11 15:01:20 -0300

    Add files via upload

---
+++ matriz_transpuesta.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	int matriz[3][3];
+	for(int i = 0 ; i < 3 ; i++){
+		for(int j = 0 ; j < 3 ; j++){
+			cout << "Ingrese un número ["<<i<<"]["<<j<<"] : ";
+			cin >> matriz[i][j];
+		}
+	}
+	cout <<"Matriz original "<<endl;
+	for(int i = 0 ; i < 3 ; i++){
+		for(int j = 0 ; j < 3 ; j++){
+			cout<<matriz[i][j];
+		}
+		cout<<endl;
+	}
+	cout<<"Matriz transpuesta "<<endl;
+	for(int i = 0 ; i < 3 ; i++){
+		for(int j = 0 ; j < 3 ; j++){
+			cout<<matriz[j][i];
+		}
+		cout<<endl;
+	}
+	return 0;
+}
+
